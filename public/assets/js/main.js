ea2a89d1feb8cbf0db9bd35f4a3814e942954e1e
alexsandrov16 <al3x5.dev@gmail.com>
2025-07-08 10:09:35 -0400

    Update main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -27,7 +27,7 @@ function scheme() {
         init() {
             if (localStorage.getItem('schemeMode') != null) {
                 if (localStorage.getItem('schemeMode') == 'dark') {
-                    this.darkMode=true;
+                    this.darkMode = true;
                     this.toggleAttr(this.theme);
                     return;
                 }
@@ -84,4 +84,112 @@ function menu() {
             });
         }
     }
+}
+
+/**
+ * SEARCH PRODUCT
+ * @returns 
+ */
+function searchProduct() {
+    return {
+        items: [],
+        selectedProduct: null,
+        products: [],
+        selectItem(item) {
+            this.selectedProduct = item;
+            this.$refs.autocomplete.value = item.Desc_Producto;
+            this.items = [];
+        },
+        hasItems() {
+            return this.items.length > 0;
+        },
+        getData(response) {
+            try {
+                this.items = JSON.parse(response);
+                this.selectedProduct = null;
+            } catch (error) {
+                console.error(error);
+                alert(error);
+            }
+        },
+        amount() {
+            if (this.selectedProduct !== null) {
+                return `En existencia: ${Math.floor(this.selectedProduct.Existencia_Actual)} ${this.selectedProduct.UM_Almacen}`;
+            } else {
+                return ''
+            }
+        },
+        checkInput() {
+            const input = this.$refs.autocomplete;
+            const nextEl = input.nextElementSibling;
+
+            if (input.value && input.classList.contains('input-error')) {
+                input.classList.remove('input-error');
+                nextEl.classList.remove('text-error');
+            }
+            nextEl.innerText = '';
+        },
+        addProduct() {
+            const inputDesc = this.$refs.autocomplete;
+            const inputCant = this.$refs.cantidad;
+            if (inputDesc.value === "") {
+                inputDesc.classList.add('input-error');
+                const nextEl = inputDesc.nextElementSibling;
+                nextEl.classList.add('text-error');
+                nextEl.innerText = 'Asegúrate de introducir todos los parámetros requeridos para que podamos realizar una búsqueda precisa.';
+                return;
+            }
+
+            if (inputCant.value === '' || inputCant.value === null || isNaN(inputCant.value) || inputCant.value <= 0) {
+                alert('Por favor, introduce para la cantidad un número válido mayor que 0.');
+                return 0;
+            }
+
+            if (this.selectedProduct == null) {
+                let lista = [];
+
+                lista.Id_Producto = null;
+                lista.UM_Almacen = 'U';
+                lista.Desc_Producto = inputDesc.value;
+                lista.Cantidad = inputCant.value;
+
+                this.products.push(lista);
+            } else {
+                delete this.selectedProduct.Existencia_Actual;
+                this.selectedProduct.Cantidad = inputCant.value;
+                this.products.push(this.selectedProduct);
+            }
+
+            inputDesc.value = '';
+            inputCant.value = '';
+
+            toggleModal(addProduct);
+        },
+        deleteProduct(i) {
+            this.products.splice(i, 1);
+        },
+        sendData() {
+            let str = JSON.stringify(this.products);
+            /*this.products.forEach(p => {
+                console.log(p.Id_Producto);
+                console.log(p.Desc_Producto);
+                console.log(p.Cantidad);
+                console.log(p.UM_Almacen);
+            });*/
+
+            return str;
+        }
+    }
+}
+
+
+/**
+ * Modal
+ */
+function toggleModal(id) {
+    if (id.hasAttribute('open')) {
+        id.removeAttribute('open');
+        return 0;
+    }
+    id.setAttribute('open', 'true');
 }
