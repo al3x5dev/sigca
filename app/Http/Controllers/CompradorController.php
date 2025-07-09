ad7a0531c52f54a1dd87ac411252bfafc1a235df
alexsandrov16 <al3x5.dev@gmail.com>
2025-07-09 14:13:15 -0400

    Create CompradorController

---
+++ app/Http/Controllers/CompradorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CompradorController extends Controller
+{
+    public function index(Request $request)
+    {
+        # code...
+    }
+}
