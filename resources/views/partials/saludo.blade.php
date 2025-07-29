45985603be599906c6c81b9a606eb57b57ab233c
alexsandrov16 <al3x5.dev@gmail.com>
2025-07-29 14:23:27 -0400

    Fix dark mode colors

--- resources/views/partials/saludo.blade.php
+++ resources/views/partials/saludo.blade.php
@@ -1,6 +1,6 @@
 <h1 class="mb-1 text-4xl font-semibold">{{$page['name']}}</h1>
-<h3 class="mb-8  font-medium flex items-center"
-    x-bind:class="{
-'text-slate-300': darkMode,
-'text-slate-500': !darkMode
-}"><img height="24" width="24" class="mr-2" src="{{asset('assets/img/hello.webp')}}" alt="👋"> Hola {{explode(' ', session('logged.nombre'))[0]}}, ¿qué piensa hacer hoy?</h3>
+<h3 class="mb-8  font-medium flex items-center @if (session('logged.theme')=='light')
+    text-slate-500
+@else
+    text-slate-300
+@endif"><img height="24" width="24" class="mr-2" src="{{asset('assets/img/hello.webp')}}" alt="👋"> Hola {{explode(' ', session('logged.nombre'))[0]}}, ¿qué piensa hacer hoy?</h3>
