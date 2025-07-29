35148408fac5a5f8cc1807062c9fed3c20c41385
alexsandrov16 <al3x5.dev@gmail.com>
2025-07-29 07:32:45 -0400

    Change table

--- database/migrations/2025_07_28_130837_create_perfiles_table.php
+++ database/migrations/2025_07_28_130837_create_perfiles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('Ajustes', function (Blueprint $table) {
+        Schema::create('Perfiles', function (Blueprint $table) {
             $table->integer('id')->nullable(false);
             $table->string('mode',50)->default('light');
             $table->boolean('notifications')->default(false);
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('ajustes');
+        Schema::dropIfExists('Perfiles');
     }
 };
