7f9f3afac11335839adfd90c8160088facc80111
alexsandrov16 <al3x5.dev@gmail.com>
2025-07-30 09:46:04 -0400

    Add CompradorController

--- app/Http/Controllers/CompradorController.php
+++ app/Http/Controllers/CompradorController.php
@@ -2,12 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Solicitud;
+use App\Models\SolicitudHistorico;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class CompradorController extends Controller
 {
-    public function index(Request $request)
+    public function dashboard(Request $request)
     {
-        # code...
+
+        $items = Solicitud::with(['usuario', 'historico.estado', 'productos'])
+            ->join(
+                'SolicitudesHistorico',
+                'SolicitudesHistorico.id_solicitud',
+                '=',
+                'Solicitudes.id'
+            )
+            ->join(
+                'Usuarios',
+                'Usuarios.id',
+                '=',
+                'Solicitudes.id_usuario'
+            )->join(
+                'Estados',
+                'Estados.id',
+                '=',
+                'SolicitudesHistorico.estado'
+            )->select(
+                'Solicitudes.*',
+                /*'Estados.estado',
+                'Usuarios.nombre as hecho_por',
+                'Usuarios.cargo',
+                'Estados.id as estado_id'*/
+            )->where('Estados.id', 1)
+            ->get();
+
+        // Reemplaza con la fecha actual
+        $lastSessionDate = session('logged.last') ?? Carbon::now();
+
+
+        // Ultimos Estados de solicitud
+        $pendiente = SolicitudHistorico::where([
+            ['estado', '=', 1],
+            ['fecha ', '>=', $lastSessionDate]
+        ])->count();
+        $completado = SolicitudHistorico::where([
+            ['estado', '=', 3],
+            ['fecha ', '>=', $lastSessionDate]
+        ])->count();
+        $eliminado = SolicitudHistorico::where([
+            ['estado', '=', 2],
+            ['fecha ', '>=', $lastSessionDate]
+        ])->count();
+        $proceso = SolicitudHistorico::where([
+            ['estado', '=', 4]
+        ])->count();
+
+
+        $data = [
+            'page' => [
+                'title' => 'pannelComprador',
+                'name' => 'Panel de Control'
+            ],
+            'estado' => [
+                'pendiente' => $pendiente,
+                'completado' => $completado,
+                'eliminado' => $eliminado,
+                'proceso' => $proceso,
+            ],
+            'items' => $items,
+        ];
+        return view('dashboard.comprador.home', $data);
     }
 }
