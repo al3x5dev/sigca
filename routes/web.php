7323b34e9ac719519e4af04f4040c7c45a4ed3b6
alexsandrov16 <al3x5.dev@gmail.com>
2025-08-28 09:27:45 -0400

    Refactor: delete old route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CompradorController;
 use App\Http\Controllers\HistoricoController;
 use App\Http\Controllers\PerfilController;
 use App\Http\Controllers\ProductoController;
@@ -27,43 +25,6 @@ Route::middleware(['guest'])->group(function () {
     
     Route::post('/login', [AuthController::class, 'login'])->name('signin');
 });
-/*
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
-
-Route::post('/login', [AuthController::class, 'login'])->name('signin');
-Route::post('/off',[AuthController::class,'logout'])->name('off');
-
-/*
- Route::get(
-    '/off',
-    function () {
-        session()->flush();
-        session()->invalidate();
-        return redirect()->route('login');
-        //return view('off');
-    }
-)->name('off');
- */
-/*
-
-// Grupo para compradores (autenticación + rol comprador)
-Route::middleware(['ldap.auth', 'no.cache'])->group(function () {
-    Route::get('/', [UsuarioController::class, 'index'])->name('dashboard');
-    Route::get('/solicitud', [SolicitudController::class, 'add'])->name('solicitud');
-    Route::post('/add-solicitud', [UsuarioController::class, 'addSolicitud'])->name('addSolicitud');
-});
-
-
-Route::prefix('api')->name('api.')->group(function () {
-    Route::get('/search-products', [ProductoController::class, 'search'])->name('producto');
-    Route::delete('/solicitud/{id}', [SolicitudController::class, 'destroy'])->name('deleteSolicitud');
-    Route::put('/history/state/{id}', [HistoricoController::class, 'state'])->name('changeStateSolicitud');
-    Route::get('/profile', [PerfilController::class, 'index'])->name('perfil');
-});
-*/
 
 
 // Ruta de logout (requiere autenticación)
