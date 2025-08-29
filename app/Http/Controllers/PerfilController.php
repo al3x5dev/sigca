34d2839b826cb128957e07889aadc9b516a7e209
alexsandrov16 <al3x5.dev@gmail.com>
2025-08-29 15:15:20 -0400

    Refactor: update PerfileController

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Auth;
 
 class PerfilController extends Controller
 {
+    private const THEMES = ['light', 'dark', 'corporate', 'business', 'synthwave', 'emerald', 'dim', 'forest', 'fantasy', 'lofi', 'wireframe', 'black', 'dracula', 'cymk', 'night', 'winter', 'dim', 'nord'];
+
     public function index(Request $request)
     {
         $theme = $request->query('m');
@@ -16,10 +18,9 @@ class PerfilController extends Controller
         $perfil = Perfil::find(Auth::id());
 
         if (is_null($perfil)) {
-             return response()->json(['error' => 'Perfil no encontrado'], 404);
+            return response()->json(['error' => 'Perfil no encontrado'], 404);
         }
-
-        if (isset($theme) &&($theme == 'dim' || $theme == 'light')) {
+        if (isset($theme) && (in_array($theme, self::THEMES))) {
             $perfil->theme = $theme;
             $perfil->save();
 
