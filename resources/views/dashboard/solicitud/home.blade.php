24ade89f84245337f41542e140e50ad671f689a5
alexsandrov16 <al3x5.dev@gmail.com>
2025-09-15 15:22:46 -0400

    Refactor: add pagination, add categoria in table, fix bugs

--- resources/views/dashboard/solicitud/home.blade.php
+++ resources/views/dashboard/solicitud/home.blade.php
@@ -7,10 +7,14 @@
 
 
 <section x-data="changeState">
-   <div class="flex justify-between items-center mb-6 ">
+    <div class="flex justify-between items-center mb-6 ">
         <h3 class="text-2xl font-semibold ">{{$page['name']}}</h3>
         <a onclick="toggleModal('newProducts')" class="btn btn-primary btn-lg md:btn-md rounded-2xl ">
-            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" /><path d="M13.5 6.5l4 4" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
+                <path d="M13.5 6.5l4 4" />
+            </svg>
             <span class="hidden md:block">Nueva Solicitud</span>
         </a>
     </div>
@@ -23,6 +27,7 @@
                 <tr>
                     <th>Número </th>
                     <th>Estado</th>
+                    <th>Categoria</th>
                     <th>Comprador</th>
                     <th>Fecha</th>
                     <th>Acciones</th>
@@ -41,6 +46,7 @@
                             {{$item->estado}}
                         </span>
                     </td>
+                    <td>{{$item->categoria}}</td>
                     <td>{{$item->getRelation('comprador')->nombre??'Sin asignar'}}</td>
                     <td>{{date('d-m-Y', strtotime($item->fecha))}}</td>
 
@@ -78,9 +84,55 @@
                 @endforeach
             </tbody>
         </table>
+
+    <!--Paginado-->
+        @if ($items->hasPages())
+        <center class=" mt-10">
+            <div class="join">
+                <button class="join-item btn"
+                    {{ $items->onFirstPage() ? 'disabled' : '' }}
+                    onclick="location.href='{{ $items->url(1) }}'">
+                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M11 7l-5 5l5 5" /><path d="M17 7l-5 5l5 5" /></svg>
+            </button>
+                <button class="join-item btn"
+                    {{ $items->onFirstPage() ? 'disabled' : '' }}
+                    onclick="location.href='{{ $items->previousPageUrl() }}'">
+                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 6l-6 6l6 6" /></svg>
+            </button>
+                <button class="join-item btn">{{$items->currentPage()}} de {{$items->lastPage()}}</button>
+
+                <button class="join-item btn"
+                    {{ $items->onLastPage() ? 'disabled' : '' }}
+                    onclick="location.href='{{ $items->nextPageUrl() }}'">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M9 6l6 6l-6 6" />
+                    </svg>
+                </button>
+                <button class="join-item btn"
+                    {{ $items->onLastPage() ? 'disabled' : '' }}
+                    onclick="location.href='{{ $items->url($items->lastPage()) }}'">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M7 7l5 5l-5 5" />
+                        <path d="M13 7l5 5l-5 5" />
+                    </svg></button>
+            </div>
+        </center>
+        @endif
+
+
+
+
+
         @else
         <center>
-            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="h-14 w-14"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2zm-7 -7h.01m3.99 0h.01" /><path d="M10 18a3.5 3.5 0 0 1 4 0" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-14 w-14">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2zm-7 -7h.01m3.99 0h.01" />
+                <path d="M10 18a3.5 3.5 0 0 1 4 0" />
+            </svg>
             <p class="text-xl">No hay datos para mostrar</p>
         </center>
         @endif
