c3c4131c27ef3ddbabfc345a1701e0965d5d6852
alexsandrov16 <al3x5.dev@gmail.com>
2025-09-29 15:01:47 -0400

    Refactor: fix paginate

--- resources/views/dashboard/solicitud/home.blade.php
+++ resources/views/dashboard/solicitud/home.blade.php
@@ -6,7 +6,7 @@
 @section('content')
 
 
-<section x-data="changeState">
+<section x-data="changeState" class="mb-20">
     <div class="flex justify-between items-center mb-6 ">
         <h3 class="text-2xl font-semibold ">{{$page['name']}}</h3>
         <a onclick="toggleModal('newProducts')" class="btn btn-primary btn-lg md:btn-md rounded-2xl ">
@@ -19,86 +19,96 @@
         </a>
     </div>
 
-    <div class="overflow-x-auto">
+    <div class="max-w-dvw w-full mr-[-2em] overflow-x-auto">
         @if (count($items)>0)
-        <table class="table">
-            <!-- head -->
-            <thead>
-                <tr>
-                    <th>Número </th>
-                    <th>Estado</th>
-                    <th>Categoria</th>
-                    <th>Comprador</th>
-                    <th>Fecha</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($items as $item)
-                <tr class="hover:bg-base-200" id="solicitud-{{$item->id}}">
-                    <td>{{$item->numero}}</td>
-                    <td>
-                        <span class="badge badge-soft
+        <div class="overflow-x-auto">
+
+            <table class="table">
+                <!-- head -->
+                <thead>
+                    <tr>
+                        <th>Número </th>
+                        <th>Estado</th>
+                        <th>Categoria</th>
+                        <th>Comprador</th>
+                        <th hidden>Fecha</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($items as $item)
+                    <tr class="hover:bg-base-200" id="solicitud-{{$item->id}}">
+                        <td>{{$item->numero}}</td>
+                        <td>
+                            <span class="badge badge-soft
                     {{ $item->estado_id == 1 ? 'badge-primary' : '' }}
                     {{ $item->estado_id == 2 ? 'badge-info' : '' }}
                     {{ $item->estado_id == 3 ? 'badge-success' : '' }}
                     {{ $item->estado_id == 4 ? 'badge-error' : '' }}">
-                            {{$item->estado}}
-                        </span>
-                    </td>
-                    <td>{{$item->categoria}}</td>
-                    <td>{{$item->getRelation('comprador')->nombre??'Sin asignar'}}</td>
-                    <td>{{date('d-m-Y', strtotime($item->fecha))}}</td>
+                                {{$item->estado}}
+                            </span>
+                        </td>
+                        <td>{{$item->categoria}}</td>
+                        <td>{{$item->getRelation('comprador')->nombre??'Sin asignar'}}</td>
+                        <td hidden>{{date('d-m-Y', strtotime($item->fecha))}}</td>
 
-                    <td class="flex gap-6">
-                        <div class="tooltip" data-tip="Visualizar">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="cursor-pointer stroke-current text-slate-400" @click="openModal({{json_encode($item->getRelation('productos'))}})">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                                <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
-                            </svg>
-                        </div>
-
-                        <form
-                            hx-delete="{{route('api.deleteSolicitud',$item->id )}}"
-                            hx-trigger="click"
-                            hx-target="#solicitud-{{ $item->id }}"
-                            hx-target="closest tr" hx-swap="outerHTML swap:1s">
-                            @csrf
-                            <div class="tooltip" data-tip="Eliminar">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="cursor-pointer stroke-current text-error">
+                        <td class="flex gap-6">
+                            <div class="tooltip" data-tip="Visualizar">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="cursor-pointer stroke-current text-slate-400" @click="openModal({{json_encode($item->getRelation('productos'))}})">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path d="M4 7l16 0" />
-                                    <path d="M10 11l0 6" />
-                                    <path d="M14 11l0 6" />
-                                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                                    <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
                                 </svg>
                             </div>
 
-                        </form>
+                            <form
+                                hx-delete="{{route('api.deleteSolicitud',$item->id )}}"
+                                hx-trigger="click"
+                                hx-target="#solicitud-{{ $item->id }}"
+                                hx-target="closest tr" hx-swap="outerHTML swap:1s">
+                                @csrf
+                                <div class="tooltip" data-tip="Eliminar">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="cursor-pointer stroke-current text-error">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path d="M4 7l16 0" />
+                                        <path d="M10 11l0 6" />
+                                        <path d="M14 11l0 6" />
+                                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                    </svg>
+                                </div>
 
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+                            </form>
 
-    <!--Paginado-->
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
         @if ($items->hasPages())
+        <!--Paginado-->
         <center class=" mt-10">
             <div class="join">
                 <button class="join-item btn"
                     {{ $items->onFirstPage() ? 'disabled' : '' }}
                     onclick="location.href='{{ $items->url(1) }}'">
-                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M11 7l-5 5l5 5" /><path d="M17 7l-5 5l5 5" /></svg>
-            </button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M11 7l-5 5l5 5" />
+                        <path d="M17 7l-5 5l5 5" />
+                    </svg>
+                </button>
                 <button class="join-item btn"
                     {{ $items->onFirstPage() ? 'disabled' : '' }}
                     onclick="location.href='{{ $items->previousPageUrl() }}'">
-                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 6l-6 6l6 6" /></svg>
-            </button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M15 6l-6 6l6 6" />
+                    </svg>
+                </button>
                 <button class="join-item btn">{{$items->currentPage()}} de {{$items->lastPage()}}</button>
 
                 <button class="join-item btn"
