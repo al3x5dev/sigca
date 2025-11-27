bdbbf477f98b1eb52c7ad12ac1cc503fec510ae6
al3x5dev <al3x5.dev@gmail.com>
2025-11-27 11:12:05 -0500

    Refactor: Intalacion de nuevos paquetes y dependencias, configuracion de paquetes instalados

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,11 @@
-//import './bootstrap';
+import './bootstrap';
 import Alpine from 'alpinejs';
+import { DataTable } from 'simple-datatables';
+import 'simple-datatables/dist/style.css';
+
+// Hacemos que DataTable esté disponible globalmente para Alpine
+window.DataTable = DataTable;
+
 window.Alpine = Alpine;
+
 Alpine.start();
