c1390a3390cd9a4bd4268deab836c6c39b1874aa
al3x5dev <al3x5.dev@gmail.com>
2025-12-09 07:48:24 -0500

    mejora en la forma de crear las solicitudes

--- resources/views/dashboard/pannel.blade.php
+++ resources/views/dashboard/pannel.blade.php
@@ -56,20 +56,21 @@
           </div>
       </a>
 
-      <a onclick="toggleModal('newProducts')" class="card bg-cyan-50 border border-cyan-200 hover:bg-cyan-100 transition-all:200ms pannel-card w-72 text-cyan-900 rounded-3xl cursor-pointer">
+      <a href="{{route('solicitud.productos')}}" class="card bg-cyan-50 border border-cyan-200 hover:bg-cyan-100 transition-all:200ms pannel-card w-72 text-cyan-900 rounded-3xl cursor-pointer">
           <div class="card-body">
             <div class="flex items-center justify-between">
 
               <div class="p-2 rounded-full bg-slate-50 border-cyan-200 border">
-                <svg xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round" class="size-8 text-cyan-500"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" /><path d="M9 17h6" /><path d="M9 13h6" /></svg>
+                
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-8 text-cyan-500"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6c0 1.657 3.582 3 8 3s8 -1.343 8 -3s-3.582 -3 -8 -3s-8 1.343 -8 3" /><path d="M4 6v6c0 1.657 3.582 3 8 3m8 -3.5v-5.5" /><path d="M4 12v6c0 1.657 3.582 3 8 3" /><path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /><path d="M20.2 20.2l1.8 1.8" /></svg>
               </div>
 
                 <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round" class="anim h-5"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M13 18l6 -6" /><path d="M13 6l6 6" /></svg>
 
             </div>
 
-            <h4 class="text-lg font-bold">Nueva Solicitud</h4>
-            <p>Inicia nuevos trámites de solicitud de materiales y recursos.</p>
+            <h4 class="text-lg font-bold">Buscar Productos</h4>
+            <p>Encuentra productos disponibles para iniciar solicitudes.</p>
           </div>
       </a>
     @endif
