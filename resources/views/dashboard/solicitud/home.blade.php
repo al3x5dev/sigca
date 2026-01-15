b7b139391ebdfd389e8158a37fa2dd75143de3aa
al3x5dev <al3x5.dev@gmail.com>
2026-01-15 11:35:45 -0500

    corregido bug alpinejs de parseo string en function

--- resources/views/dashboard/solicitud/home.blade.php
+++ resources/views/dashboard/solicitud/home.blade.php
@@ -83,7 +83,7 @@
                                 </a>
 
                                 @if ($item->ultimoEstado->estado != 4)
-                                    <button @click="$dispatch('openmodal', { id:'{{$item->id}}', numero: '{{$item->numero}}', open: true})">
+                                    <button x-on:click="$dispatch('openmodal', { id:'{{$item->id}}', numero: '{{$item->numero}}', open: true})">
                                     <div class="tooltip" data-tip="Eliminar">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="cursor-pointer stroke-current text-error">
                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
